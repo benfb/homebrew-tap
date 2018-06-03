10276c91f21c1658807988d1e1f6227ba9f13aff
goreleaserbot <goreleaser@carlosbecker.com>
2018-06-03 16:01:12 -0500

    Brew formula update for vin version v0.3.2

--- vin.rb
+++ vin.rb
@@ -1,9 +1,9 @@
 class Vin < Formula
   desc "Simple baseball CLI."
   homepage "https://github.com/benfb/vin/"
-  url "https://github.com/benfb/vin/releases/download/v0.3.1/vin_0.3.1_darwin_amd64.tar.gz"
-  version "0.3.1"
-  sha256 "b6eeec2275b0cb9d26880fc2e32e2b5aa72fcde7b3434ff39974242afc84139f"
+  url "https://github.com/benfb/vin/releases/download/v0.3.2/vin_0.3.2_darwin_amd64.tar.gz"
+  version "0.3.2"
+  sha256 "06e96b1889a6439d3842bf3cefbd05820413db71cc767ca2392dbe7903cb0bc1"
 
   def install
     bin.install "vin"
